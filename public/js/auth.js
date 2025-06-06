f76da1a77825e9d1f77f4fb88b4ba873cf48469f
hgghhggh <116894625+meumeu21@users.noreply.github.com>
2025-06-06 12:19:27 +0300

    bruh

--- public/js/auth.js
+++ public/js/auth.js
@@ -50,3 +50,63 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const registerForm = document.getElementById("registerForm");
+
+registerForm.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(registerForm);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    const response = await fetch("/auth/register", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+      },
+      body: formData,
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      alert(result.error || "Произошла ошибка при регистрации.");
+      return;
+    }
+
+    alert("Регистрация прошла успешно");
+    window.location.reload(); // или window.location.href = '/';
+  } catch (error) {
+    alert("Ошибка соединения с сервером.");
+  }
+});
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(loginForm);
+
+  try {
+    const response = await fetch("/auth/login", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+      },
+      body: formData,
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      alert(result.error || "Ошибка при входе.");
+      return;
+    }
+
+    window.location.reload(); // или window.location.href = '/';
+  } catch (error) {
+    alert("Ошибка соединения с сервером.");
+  }
+});
